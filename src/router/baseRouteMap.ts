7206f1ae8f4728dcf98a12c99251a690bb7d353a
Tien <tien@mochibits.com>
2025-03-17 19:19:44 +0800

    first commit

---
+++ src/router/baseRouteMap.ts
@@ -0,0 +1,34 @@
+
+
+import type { RouteRecordRaw } from "vue-router";
+
+const routes: RouteRecordRaw[] = [
+    {
+        path: "/",
+        name: "Home",
+        component: () => import("@/layouts/DefaultLayout.vue"),
+        // redirect: "/dashboard",
+        children: [
+            {
+                path: "/dashboard",
+                name: "dashboard",
+                component: () => import("@/views/DashBoard.vue")
+            },
+        ]
+    },
+    {
+        path: "/login",
+        name: "Login",
+        component: () => import("@/views/Login.vue"),
+    },
+    {
+        path: "/:pathMatch(.*)*",
+        name: "NotFound",
+        component: () => import("@/views/NotFound.vue"),
+    }
+
+];
+
+export default routes;
+
+
---
+++ src/views/Login.vue
@@ -0,0 +1,50 @@
+<template>
+	<el-form :model="ruleForm" ref="formRef" :rules="rules">
+		<el-form-item label="">
+			<el-input v-model="ruleForm.username"></el-input>
+		</el-form-item>
+		<el-form-item label="">
+			<el-input v-model="ruleForm.password"></el-input>
+		</el-form-item>
+		<el-form-item label="">
+			<el-button type="primary" size="default" @click="handleLogin"
+				>Login</el-button
+			>
+		</el-form-item>
+	</el-form>
+</template>
+
+<script setup lang="ts">
+import { reactive, ref } from "vue";
+import type { FormRules, FormInstance } from "element-plus";
+interface RuleForm {
+	username: string;
+	password: string;
+}
+
+const ruleForm: RuleForm = reactive({
+	username: "",
+	password: "",
+});
+
+const rules = reactive<FormRules<RuleForm>>({
+	username: [
+		{ require: true, message: "please enter username", trigger: "blur" },
+	],
+	password: [
+		{ require: true, message: "please enter password", trigger: "blur" },
+	],
+});
+
+const formRef = ref<FormInstance>();
+
+const handleLogin = () => {
+	// formRef.value?.validate(async (valid: boolean) => {
+	// 	if (valid) {
+	// 	}
+	// });
+	console.log("login");
+};
+</script>
+
+<style scoped lang="less"></style>
